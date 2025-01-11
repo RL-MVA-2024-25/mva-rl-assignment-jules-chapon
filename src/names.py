17641fe972d3fb328d840b6aad00bc8da1079410
Jules Chapon <jules.b.chapon@gmail.com>
2025-01-11 21:01:18 +0100

    [ADD] : add config, names and fast_env files

---
+++ src/names.py
@@ -0,0 +1,47 @@
+"""Names"""
+
+### MODELS
+
+MODEL = "model"
+DQN = "DQN"
+
+### ENVIRONMENT
+
+STATE_SPACE_DIMENSION = "state_space_dimension"
+ACTION_SPACE_DIMENSION = "action_space_dimension"
+
+
+### PARAMS
+
+DEVICE = "device"
+CUDA = "cuda"
+CPU = "cpu"
+
+GAMMA = "gamma"
+MEMORY_CAPACITY = "memory_capacity"
+EPSILON_MIN = "epsilon_min"
+EPSILON_MAX = "epsilon_max"
+EPSILON_DECAY_PERIOD = "epsilon_decay_period"
+EPSILON_DECAY_DELAY = "epsilon_decay_delay"
+EPSILON_STEP = "epsilon_step"
+
+GRADIENT_STEPS = "gradient_steps"
+UPDATE_FREQUENCY = "update_frequency"
+UPDATE_TAU = "update_tau"
+
+UPDATE_STRATEGY = "update_strategy"
+EMA = "ema"
+
+NB_LAYERS = "nb_layers"
+HIDDEN_SIZE = "hidden_size"
+
+NB_EPOCHS = "nb_epochs"
+
+LEARNING_RATE = "learning_rate"
+BATCH_SIZE = "batch_size"
+
+CRITERION = "criterion"
+SMOOTH_L1 = "smooth_l1"
+
+OPTIMIZER = "optimizer"
+ADAM = "adam"
